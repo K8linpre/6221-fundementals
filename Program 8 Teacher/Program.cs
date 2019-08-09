229cf26aae1b7847beb610ddb5e69ec2043b112b
K8linpre <41553940+K8linpre@users.noreply.github.com>
2019-08-09 13:29:55 +1200

    Add files via upload

---
+++ Program 8 Teacher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_8_Teacher
+{
+    class Program
+    {
+        static int size = 1000;
+        static void Main(string[] args)
+        {
+            int [] nums = new int[size];
+            Random rand = new Random();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rand.Next(1000, 10000);
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} ");
+                if ((i + 1) % 5 == 0)
+                    Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
